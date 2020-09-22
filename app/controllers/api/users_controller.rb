344d18be27c634412094913bf5ccd7d0a333c369
hernald <herndonald@gmail.com>
2020-09-22 10:30:26 -0400

    Build out backend auth views

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,34 @@
 class Api::UsersController < ApplicationController
+  before_action :require_logged_in, only: [:update, :destroy]
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :fname, :lname, :zipcode)
   end
 end
