2012338894651593fb36dbb7d5b8c08253e4de8e
hernald <herndonald@gmail.com>
2020-09-22 13:43:26 -0400

    Remove backend route Users#destroy

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -24,9 +24,6 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  def destroy
-  end
-
   private
   def user_params
     params.require(:user).permit(:email, :password, :fname, :lname, :zipcode)
