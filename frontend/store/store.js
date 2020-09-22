f1da4b45791c3518ac6e3805b973fcd1d63a3519
hernald <herndonald@gmail.com>
2020-09-22 06:44:55 -0400

    Configure store with home-brewed thunk

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,1 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from '../middleware/thunk';
+import logger from 'redux-logger';
+
+import rootReducer from '../reducers/root_reducer';
+
+export default (preloadedState = {}) => (
+  createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk, logger)
+  )
+);
