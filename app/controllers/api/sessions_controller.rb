4a9c95a65ea1ff8150f5b5f755c2c59d6453429f
hernald <herndonald@gmail.com>
2020-09-22 08:54:38 -0400

    Write session controller actions in Auth branch

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
+  before_action :require_logged_in, only: [:destroy]
+  
   def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      login(user)
+      render "api/users/show"
+    else
+      render json: ["Invalid credentials"], status: 401
+    end
   end
 
   def destroy
+    logout(current_user)
   end
 end
